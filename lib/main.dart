73edf674ff77a1464f48d35092d74f3964759c0f
AhmadMosha <ahmedgeuy159@gmail.com>
2022-10-31 07:30:53 +0200

    login page

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'modules/login/login.dart';
+
+
+
+
+
+void main() {
+    runApp( Myapp());
+  }
+
+
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
